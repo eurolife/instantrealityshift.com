9eae1a703c2024d357d05eabbeeb052746fa28db
Paula Hightower <eurolife@gmail.com>
2025-06-09 14:37:50 +0100

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,41 +1,42 @@
-import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
-import tailwindcss from "@tailwindcss/vite";
-import AutoImport from "astro-auto-import";
-import icon from "astro-icon";
-import { defineConfig } from "astro/config";
-import remarkCollapse from "remark-collapse";
-import remarkToc from "remark-toc";
-import config from "./src/config/config.json";
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import AutoImport from 'astro-auto-import';
+import icon from 'astro-icon';
+import { defineConfig } from 'astro/config';
+import remarkCollapse from 'remark-collapse';
+import remarkToc from 'remark-toc';
+import config from './src/config/config.json';
 
 let highlighter;
 async function getHighlighter() {
   if (!highlighter) {
-    const { getHighlighter } = await import("shiki");
-    highlighter = await getHighlighter({ theme: "one-dark-pro" });
+    const { getHighlighter } = await import('shiki');
+    highlighter = await getHighlighter({ theme: 'one-dark-pro' });
   }
   return highlighter;
 }
 
 // https://astro.build/config
 export default defineConfig({
-  site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
-  base: config.site.base_path ? config.site.base_path : "/",
-  trailingSlash: config.site.trailing_slash ? "always" : "never",
+  site: config.site.base_url ? config.site.base_url : 'http://examplesite.com',
+  base: config.site.base_path ? config.site.base_path : '/',
+  trailingSlash: config.site.trailing_slash ? 'always' : 'never',
   vite: { plugins: [tailwindcss()] },
+  site: 'https://instantrealityshift.com',
   integrations: [
     react(),
     sitemap(),
     AutoImport({
       imports: [
-        "@/shortcodes/Button",
-        "@/shortcodes/Accordion",
-        "@/shortcodes/Notice",
-        "@/shortcodes/Video",
-        "@/shortcodes/Youtube",
-        "@/shortcodes/Tabs",
-        "@/shortcodes/Tab",
+        '@/shortcodes/Button',
+        '@/shortcodes/Accordion',
+        '@/shortcodes/Notice',
+        '@/shortcodes/Video',
+        '@/shortcodes/Youtube',
+        '@/shortcodes/Tabs',
+        '@/shortcodes/Tab',
       ],
     }),
     mdx(),
@@ -47,12 +48,12 @@ export default defineConfig({
       [
         remarkCollapse,
         {
-          test: "Table of contents",
+          test: 'Table of contents',
         },
       ],
     ],
     shikiConfig: {
-      theme: "one-dark-pro",
+      theme: 'one-dark-pro',
       wrap: true,
     },
     extendDefaultPlugins: true,
